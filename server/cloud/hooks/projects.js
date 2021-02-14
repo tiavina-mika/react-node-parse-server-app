2c7bb6cd7fcbfe34ca6dce0c8e42025dab10d2a7
tiavina-mika <tiavinamika@gmail.com>
2021-02-14 20:06:21 +0300

    update write/read files

--- server/cloud/hooks/projects.js
+++ server/cloud/hooks/projects.js
@@ -2,12 +2,12 @@
 
 const fs = require('fs-extra');
 const path = require('path');
+
 const { projectsImagesDir } = require('../../utils/constants');
 const { isDirty, parseFunction } = require('../utils/utils');
 
-const imagesDir = '../../public/images';
 /**
- * save / edit the files to GCS's bucket
+ * save / edit the files to disk
  */
 Parse.Cloud.beforeSave('Project', 
 	parseFunction(async request => {
@@ -20,6 +20,7 @@ Parse.Cloud.beforeSave('Project',
       } catch (err) {
         console.error(err)
       }
+      return;
     }
 
 		/**
@@ -28,14 +29,23 @@ Parse.Cloud.beforeSave('Project',
 		if (isDirty(project, 'previewImage')) {
       const previewImage = project.get('previewImage');
       const file = await Parse.Cloud.httpRequest({ url: previewImage.url() });
-      const fileName = project.get('name') + previewImage.name();
+      const fileName = project.get('name').toLowerCase() + '_' + previewImage.name();
 
       const fileType = file.headers['content-type'].split('/')[0];
 
       if (fileType === 'image') {
-        /** write file to disk */
-        // create directory if not exist yet
-        await fs.outputFile(projectsImagesDir + '/' + fileName, file.buffer);
+        try {
+          /** when updating the project image, delete the old one */
+          if (project.has('previewImageId')) {
+            await fs.remove(projectsImagesDir + '/' + project.get('previewImageId'));
+          }
+
+          /** write file to disk */
+          // create directory if not exist yet
+          await fs.outputFile(projectsImagesDir + '/' + fileName, file.buffer);
+        } catch (err) {
+          console.error(err);
+        }
 
         project.unset('previewImage');
         project.set('previewImageId', fileName);          
